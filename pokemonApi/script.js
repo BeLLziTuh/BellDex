48efb4cdf447451e8dc7d014293e7e7a0cde8def
BeLLziTuh <130912488+BeLLziTuh@users.noreply.github.com>
2024-01-12 14:59:13 -0300

    Update script.js

--- pokemonApi/script.js
+++ pokemonApi/script.js
@@ -16,12 +16,14 @@ const fetchData = async (pokemon) => {
 
 const renderPokemon = async (pokemon) => {
     const data = await fetchData(pokemon)
-    pokemonName.innerHTML = data.name
-    pokemonType.innerHTML = data.types[0].type.name
-    pokemonProfilePic.src = data.sprites.front_default
-    pokemonProfilePic.classList.remove('imgPlaceholderBorder')
-    pokemonProfilePic.classList.add('imgPlaceholder')
-    imgBlur.src = data.sprites.other.showdown.front_default
+    .then(() => {
+        pokemonName.innerText = data.name
+        pokemonType.innerText = data.types[0].type.name
+        pokemonProfilePic.src = data.sprites.front_default
+        pokemonProfilePic.classList.remove('imgPlaceholderBorder')
+        pokemonProfilePic.classList.add('imgPlaceholder')
+        imgBlur.src = data.sprites.other.showdown.front_default
+    })
 }
 
 searchBtn.addEventListener('click', () => {
