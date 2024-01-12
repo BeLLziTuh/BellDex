e58d5d73bae42f8a09cefb35f7fd88e4505c3501
BeLLziTuh <130912488+BeLLziTuh@users.noreply.github.com>
2024-01-12 15:11:45 -0300

    Update script.js

--- pokemonApi/script.js
+++ pokemonApi/script.js
@@ -10,26 +10,35 @@ const apiUrl = 'https://pokeapi.co/api/v2/pokemon/'
 
 const fetchData = async (pokemon) => {
     const apiResponse = await fetch(`${apiUrl}${pokemon}`)
-    const data = await apiResponse.json()
-    return data
+    
+    if (apiResponse.status === 200) {
+        const data = await apiResponse.json()
+        return data
+    }
 }
 
 const renderPokemon = async (pokemon) => {
     const data = await fetchData(pokemon)
-    pokemonName.innerText = data.name
-    pokemonType.innerText = data.types[0].type.name
-    pokemonProfilePic.src = data.sprites.front_default
-    pokemonProfilePic.classList.remove('imgPlaceholderBorder')
-    pokemonProfilePic.classList.add('imgPlaceholder')
-    imgBlur.src = data.sprites.other.showdown.front_default
-}
 
-searchBtn.addEventListener('click', async () => {
-    await renderPokemon(searchInput.value)
-})
-
-searchInput.addEventListener('keypress', async (key) => {
-    if (key.key === 'Enter') {
-        await renderPokemon(searchInput.value)
+    if (data) {
+        pokemonName.innerHTML = data.name
+        pokemonType.innerHTML = data.types[0].type.name
+        pokemonProfilePic.src = data.sprites.front_default
+        pokemonProfilePic.classList.remove('imgPlaceholderBorder')
+        pokemonProfilePic.classList.add('imgPlaceholder')
+        imgBlur.src = data.sprites.other.showdown.front_default
+    } else {
+        pokemonName.innerHTML = 'Not Found'
+        pokemonType.innerHTML = 'Not Found'
     }
-})
+
+    searchBtn.addEventListener('click', async () => {
+        await renderPokemon(searchInput.value.toLowerCase())
+    })
+    
+    searchInput.addEventListener('keypress', async (key) => {
+        if (key.key === 'Enter') {
+            await renderPokemon(searchInput.value.toLowerCase())
+        }
+    })
+}
